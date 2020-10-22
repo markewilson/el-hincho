8566aaca9faf262703ec1b991a65cb2878b6588b
markewilson <mark.e.wilson@gmail.com>
2020-10-22 17:44:55 -0400

    reconfigures local settings

--- drupal8/web/sites/default/settings.php
+++ drupal8/web/sites/default/settings.php
@@ -792,17 +792,8 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
  * Keep this code block at the end of this file to take full effect.
  */
 #
-# if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
-#   include $app_root . '/' . $site_path . '/settings.local.php';
-# }
-$databases['default']['default'] = array (
-  'database' => 'hincho8',
-  'username' => 'drupal',
-  'password' => 'drupal',
-  'prefix' => '',
-  'host' => 'localhost',
-  'port' => '3306',
-  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
-  'driver' => 'mysql',
-);
+ if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
+   include $app_root . '/' . $site_path . '/settings.local.php';
+ }
+
 $settings['config_sync_directory'] = 'sites/default/files/config_bwmN_IWh6oCT2PTanGRMwCw0RBkv_I5IwEk6QYL33BAvo3aS4inIuXVvZ2tm3VlJb7Ljb2zduQ/sync';
