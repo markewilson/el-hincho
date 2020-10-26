c9b2c51c574ff637bb7770165ed566b05fb27d57
markewilson <mark.e.wilson@gmail.com>
2020-10-26 14:40:00 -0400

    adds platform.sh settings for drupal 7 multisites and updates routes and services with related resources

--- drupal7/web/sites/sites.php
+++ drupal7/web/sites/sites.php
@@ -5,4 +5,6 @@ $sites['aircooled.fellaini.local'] = 'aircooled';
 $sites['eheart.fellaini.local'] = 'eheart';
 $sites['figueroa.fellaini.local'] = 'figueroa';
 // PLATFORM
-
+$sites['aircooled.el-hincho.com'] = 'aircooled';
+$sites['eheart.el-hincho.com'] = 'eheart';
+$sites['figueroa.el-hincho.com'] = 'figueroa';
