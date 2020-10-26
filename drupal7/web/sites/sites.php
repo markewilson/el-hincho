acb82bdfbade118a2f3cc1c12790fe6392bedc8e
markewilson <mark.e.wilson@gmail.com>
2020-10-26 10:25:03 -0400

    cleans up unneded custom themes

---
+++ drupal7/web/sites/sites.php
@@ -0,0 +1,8 @@
+<?php
+// DRUPAL-VM
+$sites['el-hincho7.fellaini.local'] = 'default';
+$sites['aircooled.fellaini.local'] = 'aircooled';
+$sites['eheart.fellaini.local'] = 'eheart';
+$sites['figueroa.fellaini.local'] = 'figueroa';
+// PLATFORM
+
