462fead2379c5e5da69c87d9aa8f96c10ae50d4a
markewilson <mark.e.wilson@gmail.com>
2020-10-26 13:01:09 -0400

    updates drupal core and dependencies drupal/drupal (7.69.0 => 7.73.0)

--- drupal7/web/sites/sites.php
+++ drupal7/web/sites/sites.php
@@ -1,6 +1,6 @@
 <?php
 // DRUPAL-VM
-$sites['el-hincho7.fellaini.local'] = 'default';
+$sites['el-hincho7.fellaini.local'] = 'aircooled';
 $sites['aircooled.fellaini.local'] = 'aircooled';
 $sites['eheart.fellaini.local'] = 'eheart';
 $sites['figueroa.fellaini.local'] = 'figueroa';
