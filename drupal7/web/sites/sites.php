18f527af67315c3702eaacc4fba2f5370af3dc94
markewilson <mark.e.wilson@gmail.com>
2020-10-27 13:07:26 -0400

    disables drupal8 app and switches default route to drupal7

--- drupal7/web/sites/sites.php
+++ drupal7/web/sites/sites.php
@@ -5,6 +5,7 @@ $sites['aircooled.fellaini.local'] = 'aircooled';
 $sites['eheart.fellaini.local'] = 'eheart';
 $sites['figueroa.fellaini.local'] = 'figueroa';
 // PLATFORM
+$sites['el-hincho.com'] = 'aircooled';
 $sites['aircooled.el-hincho.com'] = 'aircooled';
 $sites['eheart.el-hincho.com'] = 'eheart';
 $sites['figueroa.el-hincho.com'] = 'figueroa';
