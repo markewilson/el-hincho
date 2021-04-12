a7fedd369d216f92d6c6c3c5fbd5ef7ee1ad72c8
markewilson <mark.e.wilson@gmail.com>
2021-04-12 15:24:51 -0400

    composer update and cleans up sites.php

--- drupal7/web/sites/sites.php
+++ drupal7/web/sites/sites.php
@@ -2,13 +2,11 @@
 // DRUPAL-VM
 $sites['el-hincho7.fellaini.local'] = 'aircooled';
 $sites['aircooled.fellaini.local'] = 'aircooled';
-$sites['eheart.fellaini.local'] = 'eheart';
 $sites['figueroa.fellaini.local'] = 'figueroa';
 // PLATFORM
 $sites['el-hincho.com'] = 'aircooled';
 $sites['aircooled.el-hincho.com'] = 'aircooled';
-$sites['eheart.el-hincho.com'] = 'eheart';
 $sites['figueroa.el-hincho.com'] = 'figueroa';
 $sites['melissa-figueroa.com'] = 'figueroa';
 $sites['aircooledoutfitters.com'] = 'aircooled';
-$sites['eheartclub.org'] = 'eheart';
+
